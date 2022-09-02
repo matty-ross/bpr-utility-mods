566c69d26be5eb07b7bc34fad48240a6d6250a73
Matty <54020396+matty-ross@users.noreply.github.com>
2022-09-02 19:02:13 +0200

    Add files via upload

---
+++ src/fix-lost-focus-bug.cpp
@@ -0,0 +1,30 @@
+#include "../mem.h"
+
+
+void Patch()
+{
+    // attach thread input
+    {
+        RemoveCode(0x008FB6A3, 27);
+    }
+}
+
+BOOL WINAPI DllMain(
+    _In_ HINSTANCE hinstDLL,
+    _In_ DWORD     fdwReason,
+    _In_ LPVOID    lpvReserved
+)
+{
+    switch (fdwReason)
+    {
+    case DLL_PROCESS_ATTACH:
+        DisableThreadLibraryCalls(hinstDLL);
+        Patch();
+        break;
+
+    case DLL_PROCESS_DETACH:
+        break;
+    }
+
+    return TRUE;
+}
