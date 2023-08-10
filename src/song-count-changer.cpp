7ddda0586b6aeb2f38fe6881f25668307e6f99c8
Matty <54020396+matty-ross@users.noreply.github.com>
2023-08-10 20:24:12 +0200

    Add files via upload

--- src/song-count-changer.cpp
+++ src/song-count-changer.cpp
@@ -1,22 +1,45 @@
-#include "mem.h"
+#include "../mem.h"
 
 
-int32_t g_SongCount = 90;
+struct TraxData
+{
+    int32_t SongCount = 90;
+    int32_t ParadiseCityIndex = 24;
+    int32_t FirstClassicalMusicIndex = 68;
+};
+
+
+TraxData g_TraxData;
 
 
 void Patch()
 {
-    PatchCode(0x07238A08, sizeof(g_SongCount), &g_SongCount);
-    PatchCode(0x00BE40F3, sizeof(g_SongCount), &g_SongCount);
-    //PatchCode(0x00A256E3, sizeof(g_SongCount), &g_SongCount);
-    //PatchCode(0x03943B1C, sizeof(g_SongCount), &g_SongCount);
+    // song count
+    {
+        PatchCode(0x07238A08, 4, &g_TraxData.SongCount);
+        PatchCode(0x00BE40F3, 4, &g_TraxData.SongCount);
+    }
+    
+    // paradise city index
+    {
+        PatchCode(0x00BE5100, 4, &g_TraxData.ParadiseCityIndex);
+        PatchCode(0x00BE5193, 1, &g_TraxData.ParadiseCityIndex);
+        PatchCode(0x00BE51F0, 4, &g_TraxData.ParadiseCityIndex);
+    }
+
+    // first classical music index
+    {
+        PatchCode(0x00916168, 1, &g_TraxData.FirstClassicalMusicIndex);
+    }
 }
 
-void LoadSongCount()
+void LoadTraxData()
 {
     const char* const fileName = ".\\mods\\song-count-changer\\config.ini";
 
-    g_SongCount = GetPrivateProfileIntA("Config", "SongCount", 90, fileName);
+    g_TraxData.SongCount = GetPrivateProfileIntA("Config", "SongCount", 90, fileName);
+    g_TraxData.ParadiseCityIndex = GetPrivateProfileIntA("Config", "ParadiseCityIndex", 24, fileName);
+    g_TraxData.FirstClassicalMusicIndex = GetPrivateProfileIntA("Config", "FirstClassicalMusicIndex", 68, fileName);
 }
 
 BOOL WINAPI DllMain(
@@ -29,7 +52,7 @@ BOOL WINAPI DllMain(
     {
     case DLL_PROCESS_ATTACH:
         DisableThreadLibraryCalls(hinstDLL);
-        LoadSongCount();
+        LoadTraxData();
         Patch();
         break;
 
