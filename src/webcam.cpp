fa6de446cbb5ca3b1cc99ebaf31b254bddd1f089
Matty <54020396+matty-ross@users.noreply.github.com>
2024-04-13 19:46:32 +0200

    Update webcam.cpp

--- src/webcam.cpp
+++ src/webcam.cpp
@@ -4,40 +4,12 @@
 // Save offset: 0x91C86
 
 
-__declspec(naked) void ReadOption()
-{
-    __asm
-    {
-        mov eax, dword ptr [edx + 0x7768]
-        mov [ecx + 0x4], eax
-        ret
-    }
-}
-
-__declspec(naked) void WriteOption()
-{
-    __asm
-    {
-        mov eax, dword ptr [ecx + 0x4]
-        mov [edx + 0x7768], eax
-        ret
-    }
-}
-
 void Patch()
 {
-    // read the webcam option
+    // always read camera option 'off'
     {
-        uint8_t code[] = { 0xE8, 0x00, 0x00, 0x00, 0x00, 0x90, 0x90 };
-        *reinterpret_cast<uint32_t*>(code + 0x1) = reinterpret_cast<uint32_t>(ReadOption) - 0x02F375C6 - 0x5;
-        PatchCode(0x02F375C6, sizeof(code), code);
-    }
-
-    // write the webcam option
-    {
-        uint8_t code[] = { 0xE8, 0x00, 0x00, 0x00, 0x00, 0x90, 0x90, 0x90, 0x90, 0x90 };
-        *reinterpret_cast<uint32_t*>(code + 0x1) = reinterpret_cast<uint32_t>(WriteOption) - 0x02F38636 - 0x5;
-        PatchCode(0x02F38636, sizeof(code), code);
+        static const uint8_t code[] = { 0x31, 0xC0 };
+        PatchCode(0x00A3CD84, sizeof(code), code);
     }
 }
 
